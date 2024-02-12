28a6925f2570d13692936c52248da5880fc64f9c
kiksmon21 <41356534+kiksmon21@users.noreply.github.com>
2024-02-12 19:24:18 +0000

    Deploying to gh-pages from @ kiksmon21/goit-js-hw-09@5d59267318f79d6e5fab3639ba03cfaae20a59eb 🚀

---
+++ 01-color-switcher.9e2a8750.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),d=null;a.disabled=!0,e.addEventListener("click",(function(n){e.disabled=!0,a.disabled=!1,d=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),a.addEventListener("click",(function(t){a.disabled=!0,e.disabled=!1,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.9e2a8750.js.map
